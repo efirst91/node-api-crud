f336910e4b4e6b7b8ad1c89ee93de8cf30a7440e
efirst91 <67292021+efirst91@users.noreply.github.com>
2022-12-06 17:27:12 -0500

    base project

---
+++ src/modules/peripheral-device/peripheral.router.js
@@ -0,0 +1,17 @@
+const {Router} = require("express");
+const PeripheralController = require('./infraestructure/peripheral.controller')
+/**
+ * All Routes
+ * @type Router
+ */
+const router = Router()
+
+router
+    .get('/', PeripheralController.getAllPeripheral)
+    .get('/:id', PeripheralController.getPeripheralById)
+    .post('/', PeripheralController.savePeripheral)
+    .put('/:id', PeripheralController.updatePeripheral)
+    .delete('/group', PeripheralController.deletePeripheralGroup)
+    .delete('/:id', PeripheralController.deletePeripheralById)
+
+module.exports.PeripheralRouter = router;
